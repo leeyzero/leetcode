158d3796670d6d59d90fe552237869403e8f487e
liyong19 <liyong19@baidu.com>
2020-11-03 22:37:37 +0800

    add 03-Offer-FindRepeatNumber

---
+++ algorithm/03-Offer-FindRepeatNumber/FindRepeatNumber.go
@@ -0,0 +1,26 @@
+package main
+
+// https://leetcode-cn.com/problems/shu-zu-zhong-zhong-fu-de-shu-zi-lcof/
+// 在一个长度为 n 的数组 nums 里的所有数字都在 0～n-1
+// 的范围内。数组中某些数字是重复的，但不知道有几个数字重复了，也不知道每个数字重复了几次。请找出数组中任意一个重复的数字。
+//
+// 解题思路：所有数字都是在0~n-1范围内，如果没有重复数据，则每个数字都能找到自己的位置
+// 遍历数组，对每个数字放到属于它的位置上，如果这个位置已经已经放入了，说明该数字重复了
+func findRepeatNumber(nums []int) int {
+	dup := -1
+	for i, n := range nums {
+		if n == i {
+			// n已经在正确位置，忽略
+			continue
+		}
+
+		// n所处的位置已经放置了，出现重复数字
+		if nums[n] == n {
+			dup = n
+			break
+		}
+		// 将n放入正确位置
+		nums[i], nums[n] = nums[n], nums[i]
+	}
+	return dup
+}
---
+++ algorithm/03-Offer-FindRepeatNumber/FindRepeatNumber_test.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestFindRepeatNumber(t *testing.T) {
+	type Table struct {
+		in  []int
+		out int
+	}
+	table := []Table{
+		{[]int{2, 3, 1, 0, 2, 5, 3}, 2},
+	}
+	for _, v := range table {
+		if findRepeatNumber(v.in) != v.out {
+			t.Errorf("test fail.case:%+v", v)
+		}
+	}
+}
