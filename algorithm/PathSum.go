d7bbe5ffbec75bb9b6783cc5d756602757e45077
liyong19 <liyong19@baidu.com>
2020-11-21 20:52:43 +0800

    add PathSum comment

--- algorithm/PathSum.go
+++ algorithm/PathSum.go
@@ -1,6 +1,8 @@
 package algorithm
 
 // https://leetcode-cn.com/problems/er-cha-shu-zhong-he-wei-mou-yi-zhi-de-lu-jing-lcof/
+// 解题思路：先序遍历，用一个栈记录路径，当到达叶子节点，并且路径的和为指定值时，保存路径
+// 注意golang中slice的内部实现原理，保存路径时需要做下深拷贝
 func pathSum(root *TreeNode, sum int) [][]int {
 	path := []int{}
 	ans := [][]int{}
@@ -12,7 +14,6 @@ func pathSumAux(node *TreeNode, sum int, curr int, path []int, ans *[][]int) {
 	if node == nil {
 		return
 	}
-	
 	
 	path = append(path, node.Val)
 	curr += node.Val
