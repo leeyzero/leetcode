6c48b3d7d80ae5c3fd69ab8b18508b9a7e53c996
liyong19 <liyong19@baidu.com>
2020-11-21 21:55:22 +0800

    add HammingWeight

---
+++ algorithm/HammingWeight_test.go
@@ -0,0 +1,19 @@
+package algorithm
+
+import (
+	"testing"
+)
+
+func TestHammingWeight(t *testing.T) {
+	tests := [][]int{
+		{0, 0},
+		{9, 2},
+		{8, 1},
+	}
+	for _, test := range tests {
+		in := (uint32)(test[0])
+		if got := hammingWeight(in); got != test[1] {
+			t.Errorf("hammingWeight(%v).got:%v want:%v", in, got, test[1])
+		}
+	}
+}
