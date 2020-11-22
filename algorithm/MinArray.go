a028314899f20ebe46d0eb59f3adc38b0b18ac2c
liyong19 <liyong19@baidu.com>
2020-11-22 22:15:48 +0800

    update MinArray

--- algorithm/MinArray.go
+++ algorithm/MinArray.go
@@ -1,9 +1,5 @@
 package algorithm
 
-import (
-	"math"
-)
-
 // https://leetcode-cn.com/problems/xuan-zhuan-shu-zu-de-zui-xiao-shu-zi-lcof/
 // 该题在leetcode中为简单，但要考虑的边界情况比较多，其实时比较难的
 // 解题思路：二分法
@@ -13,39 +9,17 @@ func minArray(numbers []int) int {
 		return 0
 	}
 
-	left, right := 0, len(numbers)-1
-	mid := left
-	for numbers[left] >= numbers[right] {
-		// 退出条件
-		if right - left <= 1 {
-			mid = right
-			break
-		}
-
-		mid = (left + right) >> 1
-
-		// 如果left,right,mid下边对应的值均相等，只能顺序查找最小值了
-		if numbers[left] == numbers[right] && numbers[left] == numbers[mid] {
-			return findMinInOrder(numbers[left:right+1])
-		}
-
-		if numbers[mid] >= numbers[left] {
-			// mid在左递增区间
-			left = mid
-		} else if numbers[mid] <= numbers[right] {
-			// mid在右递增区间
-			right = mid
+	low, high := 0, len(numbers)-1
+	for low < high {
+		pivot := low + (high - low) >> 1
+		if numbers[pivot] < numbers[high] {
+			high = pivot
+		} else if numbers[pivot] > numbers[high] {
+			low = pivot + 1
+		} else {
+			high--
 		}
 	}
-	return numbers[mid]
+	return numbers[low]
 }
 
-func findMinInOrder(numbers []int) int {
-	ans := math.MaxInt32
-	for _, num := range numbers {
-		if num < ans {
-			ans = num
-		}
-	}
-	return ans
-}
