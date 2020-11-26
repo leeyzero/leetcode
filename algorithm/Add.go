0b435a55e7ff8ca88ee05b1ea5942b0ad30cd9ff
leeyzero <leeyzero@163.com>
2020-11-27 00:02:10 +0800

    update Add comment

--- algorithm/Add.go
+++ algorithm/Add.go
@@ -2,7 +2,13 @@ package algorithm
 
 // https://leetcode-cn.com/problems/bu-yong-jia-jian-cheng-chu-zuo-jia-fa-lcof/
 // 解题思路：
-// 非进位求和：n = a ^ b
+// a(i)   b(i)   无进位和n(i)   进位c(i+1)
+// 0      0      0             0
+// 0      1      1             0
+// 1      0      1             0
+// 1      1      0             1
+// 非进位和跟异或运算相同，进位跟与运算相同且需左移移位
+// 非进位和：n = a ^ b
 // 进位：c = (a & b) << 1
 // (和 s) = (非进位和 n) + (进位 c), 即s = a + b转化为 s = n + c
 func add(a int, b int) int {
