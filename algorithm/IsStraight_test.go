17ac164a5d71c137aacbb0ecf03e22d5a863bc6e
leeyzero <leeyzero@163.com>
2020-11-28 14:17:12 +0800

    add IsStraight

---
+++ algorithm/IsStraight_test.go
@@ -0,0 +1,21 @@
+package algorithm
+
+import (
+	"testing"
+)
+
+func TestIsStraight(t *testing.T) {
+	tests := [][]interface{}{
+		{[]int{1, 2, 3, 4, 5}, true},
+		{[]int{0, 0, 1, 2, 5}, true},
+		{[]int{3, 4, 2, 1, 5}, true},
+		{[]int{0, 0, 2, 2, 5}, false},
+	}
+	for _, test := range tests {
+		p1 := (test[0]).([]int)
+		want := (test[1]).(bool)
+		if got := isStraight(p1); got != want {
+			t.Errorf("isStraight(%v).got:%v want:%v", p1, got, want)
+		}
+	}
+}
