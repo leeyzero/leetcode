6feb3d480c5c0096f2a49824c92a9dc1fec7d4c3
leeyzero <leeyzero@163.com>
2020-12-06 16:13:16 +0800

    update CopyRandomList

--- algorithm/CopyRandomList.go
+++ algorithm/CopyRandomList.go
@@ -1,7 +1,9 @@
 package algorithm
 
 // https://leetcode-cn.com/problems/fu-za-lian-biao-de-fu-zhi-lcof/
-// 解题思路：hash表
+// 解题思路：
+// 方法一：hash表
+// 方法二：拼接+拆分
 type RNode struct {
     Val int
     Next *RNode
