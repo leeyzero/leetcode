992a82f33cb9ab7de274047373d949cdcdda41e4
leeyzero <leeyzero@163.com>
2021-01-06 01:00:57 +0800

    update DetectCycle comment

--- algorithm/DetectCycle.go
+++ algorithm/DetectCycle.go
@@ -30,6 +30,11 @@ func detectCycle(head *ListNode) *ListNode {
 	}
 
 	// 4.慢指针从头和快指针一起走，相遇点即为入口点
+	// 这里可能比较费解，可以这样理解：
+	// 如果链表存在环，链表一共有n个节点，那么从头开始走n步，必定就会回到了环的入口点
+	// 如果从头到入口点有m个节点（不包括入口节点），环有k个节点，则有n = m + k
+	// fast先走了n步后，slow从头开始和fast同时走，当同时走了m步后，fast相当于走了k+m步回到入口点，
+	// slow走了m步也到达入口点，fast和slow 相遇
 	slow = head
 	for slow != fast {
 		slow = slow.Next
