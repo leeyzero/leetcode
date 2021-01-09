56a3fa8deeb7fe8084f1f6c58a704c923515d6e6
leeyzero <leeyzero@163.com>
2021-01-09 12:28:25 +0800

    add MaxProfix2

---
+++ algorithm/MaxProfit2.go
@@ -0,0 +1,15 @@
+package algorithm
+
+// https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-ii/
+// 1.单独交易日：今日价格 p1, 明日价格 p2, 今日买入, 明日卖出收益为 p2-p1, 负值为亏损
+// 2.连续上涨交易日：连续上涨股票价格为 p1,p2,p3,...,pn, 最大收益为 pn-p1,等价于每天买卖
+// 3.连续下降交易日：不买收益最大，即不亏损
+func maxProfit2(prices []int) int {
+	profit := 0
+	for i := 1; i < len(prices); i++ {
+		if prices[i] - prices[i-1] > 0 {
+			profit += prices[i] - prices[i-1]
+		}
+	}
+	return profit
+}
---
+++ algorithm/MaxProfit2_test.go
@@ -0,0 +1,20 @@
+package algorithm
+
+import (
+	"testing"
+)
+
+func TestMaxProfit2(t *testing.T) {
+	tests := [][]interface{}{
+		{[]int{7, 1, 5, 3, 6, 4}, 7},
+		{[]int{1, 2, 3, 4, 5}, 4},
+		{[]int{7, 6, 4, 3, 1}, 0},
+	}
+	for _, test := range tests {
+		p1 := (test[0]).([]int)
+		want := (test[1]).(int)
+		if got := maxProfit2(p1); got != want {
+			t.Errorf("maxProfit2(%v).got:%v want:%v", p1, got, want)
+		}
+	}
+}
