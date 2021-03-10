b182df5027b449f5fe3aacc1caa813ef4330c313
leeyzero <leeyzero@163.com>
2021-03-10 23:59:57 +0800

    add ReverseWords III

---
+++ algorithm/ReverseWordsIII_test.go
@@ -0,0 +1,16 @@
+package algorithm
+
+import (
+    "testing"
+)
+
+func TestReverseWordsIII(t *testing.T) {
+    tests := [][]string{
+        {"Let's take LeetCode contest", "s'teL ekat edoCteeL tsetnoc"},
+    }
+    for _, test := range tests {
+        if got := reverseWordsIII(test[0]); got != test[1] {
+            t.Errorf("reverseWordsIII(%s).got:%v want:%v", test[0], got, test[1])
+        }
+    }
+}
