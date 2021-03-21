5bdb36a8614922dc7b85c5a7194b488a0e2d4072
leeyzero <leeyzero@163.com>
2021-03-21 21:05:53 +0800

    add 88-merge

--- algorithm/twopointer/Merge_test.go
+++ algorithm/twopointer/Merge_test.go
@@ -1,13 +1,13 @@
-package algorithm
+package twopointer
 
 import (
-	"testing"
 	"reflect"
+	"testing"
 )
 
 func TestMerge(t *testing.T) {
 	tests := [][]interface{}{
-		{[]int{1, 2, 3, 0, 0, 0}, 3, []int{2, 5, 6}, 3, []int{1, 2, 2, 3, 5, 6}}, 
+		{[]int{1, 2, 3, 0, 0, 0}, 3, []int{2, 5, 6}, 3, []int{1, 2, 2, 3, 5, 6}},
 	}
 	for _, test := range tests {
 		p1 := (test[0]).([]int)
@@ -15,7 +15,7 @@ func TestMerge(t *testing.T) {
 		p3 := (test[2]).([]int)
 		p4 := (test[3]).(int)
 		want := (test[4]).([]int)
-		merge(p1, p2, p3, p4); 
+		merge(p1, p2, p3, p4)
 		if !reflect.DeepEqual(p1, want) {
 			t.Errorf("merge(%v, %v, %v, %v).got:%v want:%v", p1, p2, p3, p4, p1, want)
 		}
