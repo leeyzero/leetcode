006f9577a375d3e4eaf0292022929e0e59c82ce8
leeyzero <leeyzero@163.com>
2021-03-21 12:32:24 +0800

    add 406-reconstructQueue

--- algorithm/greedy/ReconstructQueue.go
+++ algorithm/greedy/ReconstructQueue.go
@@ -5,7 +5,10 @@ import (
 )
 
 // https://leetcode-cn.com/problems/queue-reconstruction-by-height/
-// 解题思路：有两个维度h和k，如果同时考虑两个维度，问题会变得很复杂，可以先固定一个维度，问题是先固定那个维度呢？
+// 题目：406. 根据身高重建队列
+// 难度：中等
+// 解题思路：贪心，先固定一个维度
+// 有两个维度h和k，如果同时考虑两个维度，问题会变得很复杂，可以先固定一个维度，问题是先固定那个维度呢？
 // 如果先确定k，按k排序后，会发现k的排列不符合条件，h也不符合条件，两个维度还是没有确定下来
 // 如果先确定h，按h排序后，将身高从高到低排序，身高相同，k小的站前面，此时确定了身高，也就是排前面的都比后面的高
 // 此时只需要按照k重新插入队列即可，每次插入节点时，如果节点的k大于等于当前位置，直接插入即可；如果小于当前位置，
@@ -24,11 +27,8 @@ func reconstructQueue(people [][]int) [][]int {
 	sort.Slice(people, func(i, j int) bool {
 		if people[i][0] == people[j][0] {
 			return people[i][1] < people[j][1]
-		} else if people[i][0] > people[j][0] {
-			return true
-		} else {
-			return false
 		}
+		return people[i][0] > people[j][0]
 	})
 
 	for i := 1; i < len(people); i++ {
