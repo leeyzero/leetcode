9b8298aa35d42108b00f2291905ccb53b3d52e74
leeyzero <leeyzero@163.com>
2021-03-28 10:45:59 +0800

    update 347-TopKFrequent for heap solution

--- algorithm/sort/TopKFrequent.go
+++ algorithm/sort/TopKFrequent.go
@@ -1,6 +1,8 @@
 package sort
 
 import (
+	"container/heap"
+
 	"github.com/leeyzero/leetcode/algorithm/base"
 )
 
@@ -8,8 +10,7 @@ import (
 // 题目：347. 前 K 个高频元素
 // 难度：中等
 // 要求：算法时间复杂度必须优于O(N*logN)
-// 思路：桶排序
-// 基于堆排序和快速排序的思路请参考：https://leetcode-cn.com/problems/top-k-frequent-elements/solution/qian-k-ge-gao-pin-yuan-su-by-leetcode-solution/
+// 思路：桶排序、最大堆
 func topKFrequent(nums []int, k int) []int {
 	// 先用一个hash统计每个数字出现的频率
 	hash := map[int]int{}
@@ -36,4 +37,54 @@ func topKFrequent(nums []int, k int) []int {
 		}
 	}
 	return ans
+}
+
+// 使用最大堆
+func topKFrequent2(nums []int, k int) []int {
+	hash := map[int]int{}
+	for _, n := range nums {
+		hash[n]++
+	}
+
+	h := &TopKHeap{}
+	heap.Init(h)
+	for key, val := range hash {
+		heap.Push(h, [2]int{key, val})
+		if h.Len() > k {
+			heap.Pop(h)
+		}
+	}
+
+	ans := make([]int, k)
+	for i := 0; i < k; i++ {
+		ans[k-i-1] = heap.Pop(h).([2]int)[0]
+	}
+	return ans
+}
+
+type TopKHeap [][2]int
+
+// sort 需要实现Len Less Swap接口
+func (t TopKHeap) Len() int {
+	return len(t)
+}
+
+func (t TopKHeap) Less(i, j int) bool {
+	return t[i][1] < t[j][1]
+}
+
+func (t TopKHeap) Swap(i, j int) {
+	t[i], t[j] = t[j], t[i]
+}
+
+// heap 需要实现Push和Pop接口
+func (t *TopKHeap) Push(x interface{}) {
+	*t = append(*t, x.([2]int))
+}
+
+func (t *TopKHeap) Pop() interface{} {
+	old := *t
+	x := old[t.Len()-1]
+	*t = old[:t.Len()-1]
+	return x
 }
