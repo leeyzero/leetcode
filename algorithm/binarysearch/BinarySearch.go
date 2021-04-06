4c56ff659f9e4161080c44c3e59c0dccdb404a76
leeyzero <leeyzero@163.com>
2021-04-06 12:11:53 +0800

    update binarysearch

--- algorithm/binarysearch/BinarySearch.go
+++ algorithm/binarysearch/BinarySearch.go
@@ -1,17 +1,34 @@
 package binarysearch
 
-// 对排序数组进行二分查找
+// 对排序数组进行二分查找，闭区间[0, n-1]
 func binarySearch(arr []int, target int) int {
 	lo, hi := 0, len(arr)-1
 	for lo <= hi {
 		mid := lo + (hi-lo)/2
-		if arr[mid] == target {
+		if arr[mid] < target {
+			lo = mid + 1
+		} else if arr[mid] > target {
+			hi = mid - 1
+		} else {
 			return mid
-		} else if arr[mid] < target {
+		}
+	}
+	return -1
+}
+
+// 左闭右开区间[0, n)
+func binarySearch2(arr []int, target int) int {
+	lo, hi := 0, len(arr)
+	for lo < hi {
+		mid := lo + (hi-lo)/2
+		if target > arr[mid] {
 			lo = mid + 1
 		} else {
-			hi = mid - 1
+			hi = mid
 		}
+	}
+	if lo < len(arr) && arr[lo] == target {
+		return lo
 	}
 	return -1
 }
--- algorithm/binarysearch/BinarySearch_test.go
+++ algorithm/binarysearch/BinarySearch_test.go
@@ -19,3 +19,20 @@ func TestBinarySearch(t *testing.T) {
 		}
 	}
 }
+
+func TestBinarySearch2(t *testing.T) {
+	tests := [][]interface{}{
+		{[]int{1, 2, 7, 9, 10}, 9, 3},
+		{[]int{1}, 2, -1},
+		{[]int{2}, 2, 0},
+		{[]int{3}, 2, -1},
+	}
+	for _, test := range tests {
+		arr := (test[0]).([]int)
+		target := (test[1]).(int)
+		want := (test[2]).(int)
+		if got := binarySearch2(arr, target); got != want {
+			t.Errorf("binarySearch(%v, %v).got:%v want:%v", arr, target, got, want)
+		}
+	}
+}
