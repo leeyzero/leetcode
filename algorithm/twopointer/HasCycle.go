a8750895dc9e3ef2a00ce6ec8f4d946f9d720bfe
leeyzero <leeyzero@163.com>
2021-04-10 11:51:32 +0800

    141-hasCycle

---
+++ algorithm/twopointer/HasCycle.go
@@ -0,0 +1,19 @@
+package twopointer
+
+import "github.com/leeyzero/leetcode/algorithm/base"
+
+// https://leetcode-cn.com/problems/linked-list-cycle/
+// 141. 环形链表
+// 难度：简单
+// 思路：快慢指针
+func hasCycle(head *base.ListNode) bool {
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+		if slow == fast {
+			return true
+		}
+	}
+	return false
+}
--- algorithm/twopointer/HasCycle_test.go
+++ algorithm/twopointer/HasCycle_test.go
@@ -1,14 +1,16 @@
-package algorithm
+package twopointer
 
 import (
 	"testing"
+
+	"github.com/leeyzero/leetcode/algorithm/base"
 )
 
 func TestHasCycle(t *testing.T) {
 	p1 := []int{3, 2, 0, -4}
 	p2 := 1
-	head := makeLinkList(p1)
-	forward(head, 3).Next = head
+	head := base.MakeLinkList(p1)
+	base.Forward(head, 3).Next = head
 	if got, want := hasCycle(head), true; got != want {
 		t.Errorf("hasCycle(%v, %v).got:%v want:%v", p1, p2, got, want)
 	}
