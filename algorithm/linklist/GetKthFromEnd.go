dbf2fc5b37bb7719d9af9671ced3e843db559bac
leeyzero <leeyzero@163.com>
2021-04-22 23:38:24 +0800

    update O-22-GetKthFromEnd

--- algorithm/linklist/GetKthFromEnd.go
+++ algorithm/linklist/GetKthFromEnd.go
@@ -1,11 +1,13 @@
 package algorithm
 
 // https://leetcode-cn.com/problems/lian-biao-zhong-dao-shu-di-kge-jie-dian-lcof/
+// 题目：剑指 Offer 22. 链表中倒数第k个节点
+// 难度：简单
 // 思路：快指针向前走k步，然后慢指针和快指针一起走，直到快指针到达结尾
 func getKthFromEnd(head *ListNode, k int) *ListNode {
 	fast, slow := head, head
 	step := 0
-	for (step < k) && (fast != nil) {
+	for step < k && fast != nil {
 		fast = fast.Next
 		step++
 	}
