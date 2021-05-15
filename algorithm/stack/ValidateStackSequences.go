8ad716dd21eec485dabc499c31791a3ac30e3893
leeyzero <leeyzero@163.com>
2021-05-15 18:49:17 +0800

    update O-31-ValidateStackSequences

--- algorithm/stack/ValidateStackSequences.go
+++ algorithm/stack/ValidateStackSequences.go
@@ -1,7 +1,9 @@
-package algorithm
+package stack
 
 // https://leetcode-cn.com/problems/zhan-de-ya-ru-dan-chu-xu-lie-lcof/
-// 解题思路：要充分了解栈的特性，使用一个栈来保存pushed的元素，每次push后，将top值和poped的顶部元素比较
+// 题目：剑指 Offer 31. 栈的压入、弹出序列
+// 难度：中等
+// 思路：要充分了解栈的特性，使用一个栈来保存pushed的元素，每次push后，将top值和poped的顶部元素比较
 // 如果相等，出栈，继续比较下一个栈顶元素
 // 如果不相等，继续入栈，知道pushed中的所有元素均已入栈
 // 如果最后栈为空，则说明顺序时合法的
--- algorithm/stack/ValidateStackSequences_test.go
+++ algorithm/stack/ValidateStackSequences_test.go
@@ -1,4 +1,4 @@
-package algorithm
+package stack
 
 import (
 	"testing"
