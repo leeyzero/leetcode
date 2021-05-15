a3c0dc0073707bff265f5aac19c1e34508dd43d3
leeyzero <leeyzero@163.com>
2021-05-15 19:04:59 +0800

    update 9-IsPalindrome

--- algorithm/math/IsPalindrome.go
+++ algorithm/math/IsPalindrome.go
@@ -1,6 +1,9 @@
-package algorithm
+package math
 
 // https://leetcode-cn.com/problems/palindrome-number/
+// 题目：9. 回文数
+// 难度：简单
+// 思路：对于负责，一定不是回文；对于正数，对数字进行逆置后判断跟原数是否相等
 func isPalindrome(x int) bool {
 	if x < 0 {
 		return false
