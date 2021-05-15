67c7d1a89c13a92804eb5a8ec3489f1b55cddf80
leeyzero <leeyzero@163.com>
2021-05-15 18:46:10 +0800

    update 20-IsValid

--- algorithm/stack/IsValid.go
+++ algorithm/stack/IsValid.go
@@ -1,6 +1,13 @@
-package algorithm
+package stack
 
 // https://leetcode-cn.com/problems/valid-parentheses/
+// 题目：20. 有效的括号
+// 难度：简单
+// 描述：给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
+// 有效字符串需满足：
+// 左括号必须用相同类型的右括号闭合。
+// 左括号必须以正确的顺序闭合。
+// 思路：栈
 func isValid(s string) bool {
 	hash := map[byte]byte{
 		'(': ')',
