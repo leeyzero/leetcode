e590ca9030e6f57d6ffe357719141b45e5ded3e2
leeyzero <leeyzero@163.com>
2021-05-15 19:52:05 +0800

    update O-57-FindContinuousSequence

--- algorithm/slidingwindow/FindContinuousSequence.go
+++ algorithm/slidingwindow/FindContinuousSequence.go
@@ -1,9 +1,13 @@
-package algorithm
+package slidingwindow
 
 // https://leetcode-cn.com/problems/he-wei-sde-lian-xu-zheng-shu-xu-lie-lcof/
-// 解题思路：
+// 题目：剑指 Offer 57 - II. 和为s的连续正数序列
+// 难度：简单
+// 描述：输入一个正整数 target ，输出所有和为 target 的连续正整数序列（至少含有两个数）。
+// 序列内的数字由小到大排列，不同序列按照首个数字从小到大排列。
+// 思路：
 // 1. 最直观的做法时穷取法，从i = [1,target/2]，依次判断已i为起始的连续序列
-// 2. 活动窗口
+// 2. 滑动窗口
 func findContinuousSequence(target int) [][]int {
 	ans := [][]int{}
 	if target < 3 {
