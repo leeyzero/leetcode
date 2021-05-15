ea7a3d5158f925d74b457b7ccf5951da76275fa1
leeyzero <leeyzero@163.com>
2021-05-15 19:35:55 +0800

    update O-53-MissingNumber

--- algorithm/binarysearch/MissingNumber.go
+++ algorithm/binarysearch/MissingNumber.go
@@ -1,11 +1,13 @@
-package algorithm
+package binarysearch
 
 // https://leetcode-cn.com/problems/que-shi-de-shu-zi-lcof/
-// 解题思路：二分法
+// 题目：剑指 Offer 53 - II. 0～n-1中缺失的数字
+// 难度：简单
+// 思路：二分法
 func missingNumber(nums []int) int {
 	left, right := 0, len(nums)-1
 	for left <= right {
-		mid := left + (right-left) >> 1
+		mid := left + (right-left)>>1
 		if nums[mid] == mid {
 			left = mid + 1
 		} else {
@@ -14,6 +16,6 @@ func missingNumber(nums []int) int {
 	}
 	// 结束循环后left指向的位置有两种情况：
 	// 1. nums[left] != left (left < len(nums))
-	// 2. left == len(nums) 
+	// 2. left == len(nums)
 	return left
 }
