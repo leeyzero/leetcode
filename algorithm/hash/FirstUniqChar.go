de5b38260fb633e88e66d3572d6363f7eb55afb2
leeyzero <leeyzero@163.com>
2021-05-16 11:45:57 +0800

    update O-50-FirstUniqChar

--- algorithm/hash/FirstUniqChar.go
+++ algorithm/hash/FirstUniqChar.go
@@ -1,6 +1,10 @@
-package algorithm
+package hash
 
 // https://leetcode-cn.com/problems/di-yi-ge-zhi-chu-xian-yi-ci-de-zi-fu-lcof/
+// 题目：剑指 Offer 50. 第一个只出现一次的字符
+// 难度：简单
+// 描述：在字符串 s 中找出第一个只出现一次的字符。如果没有，返回一个单空格。 s 只包含小写字母。
+// 思路：hash
 func firstUniqChar(s string) byte {
 	hash := make([]int, 128)
 	for i := 0; i < len(s); i++ {
--- algorithm/hash/FirstUniqChar_test.go
+++ algorithm/hash/FirstUniqChar_test.go
@@ -1,4 +1,4 @@
-package algorithm
+package hash
 
 import (
 	"testing"
