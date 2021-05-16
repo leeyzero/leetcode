2466d89e3ffcb1f42024f674aa47c8ffcd7b6e82
leeyzero <leeyzero@163.com>
2021-05-16 12:01:19 +0800

    update O-04-FindNumberIn2DArray

--- algorithm/array/FindNumberIn2DArray.go
+++ algorithm/array/FindNumberIn2DArray.go
@@ -1,7 +1,10 @@
-package algorithm
+package array
 
 // https://leetcode-cn.com/problems/er-wei-shu-zu-zhong-de-cha-zhao-lcof/
-// 解题思路：选取数组右上角的数字，如果该数字等于要查找的数字，则查找结束；
+// 题目：剑指 Offer 04. 二维数组中的查找
+// 难度：中等
+// 描述：在一个 n * m 的二维数组中，每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。请完成一个高效的函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
+// 思路：选取数组右上角的数字，如果该数字等于要查找的数字，则查找结束；
 // 如果该数字大于要查找的数字，则剔除这个数字所在列；
 // 如果该数字小于要查找的数字，则剔除这个数字所在行；
 func findNumberIn2DArray(matrix [][]int, target int) bool {
--- algorithm/array/FindNumberIn2DArray_test.go
+++ algorithm/array/FindNumberIn2DArray_test.go
@@ -1,4 +1,4 @@
-package algorithm
+package array
 
 import (
 	"testing"
@@ -6,11 +6,11 @@ import (
 
 func TestFindNumberIn2DArray(t *testing.T) {
 	matrix := [][]int{
-		{1,   4,  7, 11, 15},
-  		{2,   5,  8, 12, 19},
-  		{3,   6,  9, 16, 22},
-  		{10, 13, 14, 17, 24},
-  		{18, 21, 23, 26, 30},
+		{1, 4, 7, 11, 15},
+		{2, 5, 8, 12, 19},
+		{3, 6, 9, 16, 22},
+		{10, 13, 14, 17, 24},
+		{18, 21, 23, 26, 30},
 	}
 	tests := [][]interface{}{
 		{matrix, 5, true},
